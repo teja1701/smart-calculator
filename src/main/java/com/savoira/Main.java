7b95fc31f69710756d4463483f955eb0391cd32b
tejaswini <tejawinihoney1701@gmail.com>
2026-08-08 12:27:18 +0530

    Renamed the main file

--- src/main/java/com/savoira/Main.java
+++ src/main/java/com/savoira/Main.java
@@ -2,9 +2,9 @@ package com.savoira;
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Smart calculator");
