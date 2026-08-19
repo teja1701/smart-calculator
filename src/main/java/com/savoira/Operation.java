3d0d0b534b513047cc54df9e5e58afa4439e3459
tejaswini <tejawinihoney1701@gmail.com>
2026-08-19 21:43:20 +0530

    working on phase4 work

---
+++ src/main/java/com/savoira/Operation.java
@@ -0,0 +1,44 @@
+package com.savoira;
+
+public class Operation {
+
+    private double firstNumber;
+    private String operator;
+    private double secondNumber;
+
+    /**
+     *
+     * @param firstNumber - first number
+     * @param operator - operator
+     * @param secondNumber - second number
+     */
+    public Operation(double firstNumber, String operator, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.operator = operator;
+        this.secondNumber = secondNumber;
+    }
+
+    /**
+     *
+     * @return the first number
+     */
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    /**
+     *
+     * @return the operator
+     */
+    public String getOperator() {
+        return operator;
+    }
+
+    /**
+     *
+     * @return the second number
+     */
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+}
